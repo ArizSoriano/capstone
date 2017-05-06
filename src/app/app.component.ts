c4ec71c8747fae3c69298c99da73f78d5de507c0
Ariz Soriano <imarizsoriano@gmail.com>
2017-05-07 02:23:59 +0800

    fixed notification push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,7 @@ export class MyApp {
   pushsetup() {
     const options: PushOptions = {
      android: {
-         senderID: '<yoursenderidhere>'
+         senderID: '8842581918'
      },
      ios: {
          alert: 'true',
